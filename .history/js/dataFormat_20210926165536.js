72b905ab8384cbc74e61494c1178a06cee8ecaa7
FerryCorleone <luojin97@126.com>
2021-09-27 16:50:51 +0800

    删除img文件夹

---
+++ .history/js/dataFormat_20210926165536.js
@@ -0,0 +1,27 @@
+
+var geoData = {
+
+    BLS: {
+		0 : {
+			lithology : "中风化板岩夹千枚岩",
+			color ： "#0BF131",
+			pic : "1.png"
+		},
+		1 : {
+			lithology : "强风化板岩夹千枚岩",
+			color ： "#0BC8F1",
+			pic : "2.png"
+		},
+		2 : {
+			lithology : "碎石土",
+			color ： "#F6ABAF",
+			pic : "3.png"
+		},
+		fractures : {
+			lithology : "构造破碎带",
+			color ： "#F884E1",
+			pic : "4.png"
+		}
+    },
+    
+};
